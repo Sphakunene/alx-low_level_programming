40e2478f877a91d8570206629d30ac9a8d0523a0
Sphakunene <sphacol@gmail.com>
2022-09-11 12:40:17 -0700

    added code for printing a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main- entry point
+ *this function print a string of words
+ *Return always 0 (success)
+ */
+
+int main(void)
+{
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
+
+}
