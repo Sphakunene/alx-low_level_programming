7a84a2c3d4b5e5cbd3bdfbd77dbd3b152af1ec91
Sphakunene <sphacol@gmail.com>
2022-09-14 23:06:39 -0700

    added print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *main- entry point
  *
@@ -8,12 +9,12 @@
 
 int main(void)
 {
-	int num = 0;
+	int num;
 
-	for (num = 0 ; num <= 9 ; num++)
+	for (num = '0' ; num <= '9'; num++)
 	{
 	putchar(num);
 	}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
