b8c0b89fc01a93191ebca8de3a6fbcf31beabff2
Sphakunene <sphacol@gmail.com>
2022-09-19 04:17:42 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * add - multiplies two integers and returns the result.
+ * mul - multiplies two integers and returns the result.
  * @a: int to be multiplied to b
  * @b: int to be multiplied to c
  * Return: the product of a and b
