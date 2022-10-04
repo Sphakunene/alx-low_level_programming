75803a0852f9f6f197249c876e5ae394c7453843
Sphakunene <sphacol@gmail.com>
2022-10-04 02:33:10 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+/**
+ * factorial - function return the factorial of the given number
+ * @n : number to find a factor
+ * return : factorial of a number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
